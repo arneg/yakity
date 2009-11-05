a453302cccdd8f433fb6862129fcf2855de9827f
Arne Goedeke <el@laramies.com>
2009-11-05 17:20:29 +0100

    more users per process

--- load_test.sh
+++ load_test.sh
@@ -1,14 +1,20 @@
-URL=$1
-MIN=$2
-MAX=$3
+MURL=$1
+BURL=$2
+N=$3
+EACH=$4
 
 PID=""
 SIGTERM=15
 
-for a in `seq $MIN $MAX`
-do 
-	pike -M YakityChat-local/lib/ client.pike $URL $a > stats/$a.log &
+for num in `seq 0 $N`
+do
+	echo "$num"
+	MIN=$(echo "$num * $EACH"|bc)
+	MAX=$(echo "($num + 1) * $EACH - 1"|bc)
+	echo "pike -M YakityChat-local/lib/ client.pike $MURL $BURL $MIN $MAX > stats/$num.plot"
+	pike -M YakityChat-local/lib/ client.pike $MURL $BURL $MIN $MAX > stats/$num.plot &
 	PID="$PID $!" 
+	sleep 2
 done
 
 echo "started processes $PID"
