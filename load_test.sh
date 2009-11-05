ca5c43beab4f3b65af9ca8ebda360dddb6f23453
Arne Goedeke <el@laramies.com>
2009-11-05 19:51:37 +0100

    .

--- load_test.sh
+++ load_test.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 MURL=$1
 BURL=$2
 N=$3
@@ -11,10 +12,12 @@ do
 	echo "$num"
 	MIN=$(echo "$num * $EACH"|bc)
 	MAX=$(echo "($num + 1) * $EACH - 1"|bc)
-	echo "pike -M YakityChat-local/lib/ client.pike $MURL $BURL $MIN $MAX > stats/$num.plot"
-	pike -M YakityChat-local/lib/ client.pike $MURL $BURL $MIN $MAX > stats/$num.plot &
+	WAIT=$(echo "($EACH * 0.01) + 2"|bc)
+	echo "pike -M ../lib/ client.pike $MURL $BURL $MIN $MAX > ../stats/$num.plot"
+	pike -M ../lib/ client.pike $MURL $BURL $MIN $MAX > ../stats/$num.plot &
 	PID="$PID $!" 
-	sleep 2
+	echo "sleep $WAIT"
+	sleep $WAIT
 done
 
 echo "started processes $PID"
